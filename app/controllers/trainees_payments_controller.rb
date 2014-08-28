442376dee3b8a1a724407491779f6b49d380cbc1
Jose Luiz Tafarel <jltafarel@gmail.com>
2014-08-28 16:18:28 -0300

    alterações feitas no servidor

--- app/controllers/trainees_payments_controller.rb
+++ app/controllers/trainees_payments_controller.rb
@@ -6,12 +6,12 @@ class TraineesPaymentsController < ApplicationController
     @users = {
       4=>{:hora=>6.0, :ciee=>550}, #Lucão
       22=>{:hora=>8.0, :ciee=>550}, #Tafarel
-      27=>{:hora=>7.5, :ciee=>0},   #Mitrut
+      27=>{:hora=>7.5, :ciee=>550},   #Mitrut
       32=>{:hora=>7.5, :ciee=>550}  #Rafagnin
     }
 
     @horas = {}
-    time_entries = TimeEntry.on_project(Project.find(10), true).spent_between(Time.mktime(@ano,@mes-1,1), Time.mktime(@ano,@mes,1))
+    time_entries = TimeEntry.on_project(Project.find(10), true).spent_between(Time.mktime(@ano,@mes-1,1), Time.mktime(@ano,@mes,1) - 1.day)
     time_entries.group_by{|t|t.user_id}.each{|a,c| @horas[a] = c.sum{|k| k.hours}}
   end
 
